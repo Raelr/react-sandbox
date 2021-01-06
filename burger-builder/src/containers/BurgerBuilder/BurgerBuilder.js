85c4a9c708875a28e38d6b9ef2526c9cab3a5605
Raelr <aryehzinn@hotmail.com>
2021-01-07 10:50:48 +1100

    Added confim and cancel buttons to modal

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -72,6 +72,10 @@ class BurgerBuilder extends Component {
         this.setState({purchasing : false});
     }
 
+    purchaseContinueHandler = () => {
+        alert('PAYMENT OPTIONS')
+    }
+
     render () {
         const disableInfo = {
             ...this.state.ingredients
@@ -83,7 +87,11 @@ class BurgerBuilder extends Component {
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}> 
-                    <OrderSummary ingredients={this.state.ingredients}/>
+                    <OrderSummary 
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                        price={this.state.totalPrice}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BurgerControls
