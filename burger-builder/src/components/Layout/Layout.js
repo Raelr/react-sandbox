0b7369a36d695895bb54c0a6646602e62b9de8d4
Raelr <aryehzinn@hotmail.com>
2021-01-07 15:56:32 +1100

    Added a side bar for mobile

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Aux from '../../hoc/Auxiliary'
 import classes from './Layout.module.css'
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
-const layout = (props) => (
-    <Aux>
-        <Toolbar/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+const Layout = (props) => {
+    
+    const [useSideDrawer, setUseSideDrawer] = useState(false);
 
-export default layout;
+    const sideDrawerClosedHandler = () => {
+        setUseSideDrawer(false)
+    };
+
+    const toggleSideDrawerHandler = () => {
+        setUseSideDrawer(!useSideDrawer)
+    }
+
+    return (
+        <Aux>
+            <Toolbar toggleSideDrawer={toggleSideDrawerHandler}/>
+            <SideDrawer open={useSideDrawer} closed={sideDrawerClosedHandler}/>
+            <main className={classes.Content}>
+                {props.children}
+            </main>
+        </Aux>
+    );
+};
+
+export default Layout;
