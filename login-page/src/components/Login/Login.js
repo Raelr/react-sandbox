f2cf82b266e9f3977f570433cae42816b6d08bdf
Aryeh <aryehzinn@hotmail.com>
2021-03-10 16:56:16 +1100

    Removed some unneeded code and added a registration section

--- login-page/src/components/Login/Login.js
+++ login-page/src/components/Login/Login.js
@@ -3,9 +3,10 @@ import classes from './Login.module.css'
 import LoginUser from './LoginUser/LoginUser'
 import axios from '../../axios-users'
 
-const Login = (props) => {
+const Login = () => {
 
     const [userInfo, setUserInfo] = useState({username: "", password: ""})
+    const [isRegisteringUser, setIsRegisteringUser] = useState(false)
 
     const loginUserHandler = (event) => {
         event.preventDefault()
@@ -35,14 +36,33 @@ const Login = (props) => {
         setUserInfo({username: userInfo.username, password: input})
     }
 
+    const registerUserHandler = () => {
+        setIsRegisteringUser(!isRegisteringUser)
+    }
+
+    let login = <LoginUser 
+            loginHandler={loginUserHandler} 
+            updateUsernameHandler={userNameUpdateHandler}
+            passwordUpdateHandler={passwordUpdateHander}
+        />
+    
+    let loginMessage = 'Don\'t have an account? Register';
+
+    if (isRegisteringUser) {
+        login = <p>Register User</p>
+        loginMessage = 'Already have an account? Login '
+    }
+
     return (
         <div className={classes.Login}>
-        <h1>LOGIN PAGE</h1>
-        <LoginUser 
-            loginHandler={loginUserHandler}
-            updateUsernameHandler={userNameUpdateHandler}
-            passwordUpdateHandler={passwordUpdateHander}    
-        />
+            <h1>LOGIN PAGE</h1>
+            {login}
+            <p style={{position:'absolute',bottom:0,left:'15%',textAlign:'center'}}>
+                {loginMessage} 
+                <span>
+                    <p style={{display:'inline',color:'blue'}} onClick={registerUserHandler}> Here!</p>
+                </span>
+            </p>
         </div>
     );
 };
