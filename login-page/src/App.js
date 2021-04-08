ded81e69f4a5cb68c80c42fe1a4889f9b7a2922d
Aryeh <aryehzinn@hotmail.com>
2021-04-08 12:12:11 +1000

    refactor: removed hoc directory and added redux to burger builder

--- login-page/src/App.js
+++ login-page/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import Layout from './hoc/Layout/Layout'
 import Login from './components/Login/Login';
 
 function App() {
   return (
-      <Layout>
-        <Login />
-      </Layout>
+    <main style={{"marginTop": "72px"}}>
+      <Login />
+    </main>    
   );
 }
 
